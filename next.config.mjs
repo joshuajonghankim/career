11419289e2cc2b11a15ea3f5ba6c66868b20f93b
joshuajonghankim <joshuajonghankim@gmail.com>
2024-06-24 00:38:56 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     //these lines may solve the image deployment error
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/Resume/' : '',
     basePath: '/Resume',
     assetPrefix: '/Resume/',
-    trailingSlash: true,
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/Resume/' : '',
+    trailingSlash: true,    
     //
 };
 
