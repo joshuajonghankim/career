3eb35b8929e77436e848d62eae7a49675eb41e94
joshuajonghankim <joshuajonghankim@gmail.com>
2024-06-24 00:30:41 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    //these lines may solve the image deployment error
     basePath: '/Resume',
     assetPrefix: '/Resume/',
     trailingSlash: true,
