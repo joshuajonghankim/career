435423d8ddc5e80fd535d013f950858d9181e646
joshuajonghankim <joshuajonghankim@gmail.com>
2024-06-24 00:29:59 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: '/Resume',
+    assetPrefix: '/your-repo-name/',
+    trailingSlash: true,
 };
 
 export default nextConfig;
