5922fccbbc17422e84f6edeb2fe9f2fe639c2be7
joshuajonghankim <joshuajonghankim@gmail.com>
2024-06-24 00:41:45 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,10 @@
 const nextConfig = {
     //these lines may solve the image deployment error
     assetPrefix: process.env.NODE_ENV === 'production' ? '/Resume/' : '',
+    images: {
+        loader: 'akamai',
+        path: '',
+      },
     basePath: '/Resume',
     assetPrefix: '/Resume/',
     trailingSlash: true,    
