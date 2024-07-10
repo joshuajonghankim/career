b37877122e723e7a8520389b4dc7cd3befba54b9
joshuajonghankim <joshuajonghankim@gmail.com>
2024-07-10 08:19:22 -0400

    resume

--- app/page.js
+++ app/page.js
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="border border-red-600 min-h-screen w-screen flex flex-col items-center justify-between p-20">
+    <main className="flex flex-col gap-3 items-center justify-between p-10 md:p-20">
       {/* photo  */}
-      <div className="border border-red-600">
+      <div className="border border-gray-400">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="https://raw.githubusercontent.com/joshuajonghankim/Resume/main/public/selfie.jpg"
@@ -16,207 +16,80 @@ export default function Home() {
       </div>
 
       {/* name and introduction */}
-      <div className="border border-red-600">
-        <div className="border border-red-600 text-4xl">
+      <div className=" flex flex-col gap-2">
+        <div className="text-4xl">
           Joshua Jonghan Kim
         </div>
-        <div className="border border-red-600 mt-2 text-1xl">
-          Introduction
+        <div className="text-center text-1xl">
+          Junior Developer
         </div>
       </div>
 
       {/* body */}
-      <div className="border border-red-600 w-full flex flex-row">
-        {/* left body */}
-        <div className="border border-red-600 w-1/5">
-          <div className="border border-red-600">
-            <h1 className="font-bold">
-              Contact
-            </h1>
-            <p className="text-sm">
-              <br />
-              347-909-0683
-            </p>
-          </div>
+      <div className=" w-full flex flex-col gap-2">
+        <div className="border border-gray-400">
+          <h1 className="font-bold">
+            Contact
+          </h1>
+          <p className="text-sm text-wrap">
+            <br />
+            347-909-0683
+            <br />
+            joshuajonghankim@gmail.com
+          </p>
         </div>
 
-        {/* right body */}
-        <div className="border border-red-600 w-4/5">
-          <div className="border border-red-600">
-            <h1 className="font-bold">
-              Education
-            </h1>
-            <p className="text-sm">
-              <br />
-              2015 - 2017 :<br />
-              Kyungpook National University in South Korea<br />
-              Energy Engineering (transferred to CS major)<br />
-              <br />
-              2019 - 2024 :<br />
-              City University of New York, Queens College<br />
-              Computer Science, BA
-            </p>
-          </div>
+        <div className="border border-gray-400">
+          <h1 className="font-bold">
+            Education
+          </h1>
+          <p className="text-sm">
+            <br />
+            2015 - 2017 :<br />
+            Kyungpook National University in South Korea<br />
+            Energy Engineering (transferred to CS major)<br />
+            <br />
+            2019 - 2024 :<br />
+            City University of New York, Queens College<br />
+            Computer Science, BA
+          </p>
+        </div>
 
-          <div className="border border-red-600">
-            <h1 className="font-bold">
-              Experiences
-            </h1>
-            <p className="text-sm">
-              <br />
-              2017 - 2019 :<br />
-              F-15K Assistant Mechanic in the South Korean Air Force
-            </p>
-          </div>
+        <div className="border border-gray-400">
+          <h1 className="font-bold">
+            Experiences
+          </h1>
+          <p className="text-sm">
+            <br />
+            2017 - 2019 :<br />
+            F-15K Assistant Mechanic in the South Korean Air Force
+          </p>
+        </div>
 
-          <div className="border border-red-600">
-            <h1 className="font-bold">
-              Skills
-            </h1>
-            <p className="text-sm">
-              <br />
-              JAVA<br />
-              C++<br />
-              Python<br />
-            </p>
-          </div>
+        <div className="border border-gray-400">
+          <h1 className="font-bold">
+            Skills
+          </h1>
+          <p className="text-sm">
+            <br />
+            JAVA<br />
+            C++<br />
+            Python<br />
+          </p>
+        </div>
 
-          <div className="border border-red-600">
-            <h1 className="font-bold">
-              Languages
-            </h1>
-            <p className="text-sm">
-              <br />
-              English<br />
-              Korean<br />
-              Japanese - intermediate<br />
-            </p>
-          </div>
+        <div className="border border-gray-400">
+          <h1 className="font-bold">
+            Languages
+          </h1>
+          <p className="text-sm">
+            <br />
+            English<br />
+            Korean<br />
+            Japanese - intermediate<br />
+          </p>
         </div>
       </div>
-
-
-
-
-
-
-
-      {/* <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Joshua Jonghan Kim's resume&nbsp;
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="https://raw.githubusercontent.com/joshuajonghankim/Resume/main/public/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex flex-col before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="https://raw.githubusercontent.com/joshuajonghankim/Resume/main/public/selfie.jpg"
-          alt="selfie"
-          width={180}
-          height={37}
-          priority
-        />
-
-        <br />
-        <div>
-          Name: Joshua Jonghan kim<br />
-          Age: 29<br />
-        </div>
-        <br />
-        <div>
-          Education<br />
-          Computer Science, BA<br />
-          City University of New York, Queens College (2019 - 2024)
-        </div>
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div> */}
 
       <footer className="">
       </footer>
