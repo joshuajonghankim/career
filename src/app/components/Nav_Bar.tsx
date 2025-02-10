742dd3bdb2d307cb65e9079e2fdaed9f32965643
joshuajonghankim <joshuajonghankim@gmail.com>
2025-02-10 00:08:41 -0500

    Update Nav_Bar.tsx

--- src/app/components/Nav_Bar.tsx
+++ src/app/components/Nav_Bar.tsx
@@ -4,7 +4,7 @@ export default function Nav_Bar() {
   return (
     <header className="absolute top-0 left-0 right-0 flex justify-center p-4 bg-white dark:bg-black z-10">
       <nav className="flex gap-8 text-sm font-semibold">
-        <Link href="/" className="hover:underline">About Me</Link>
+        <Link href="/" className="hover:underline">Joshua Jonghan Kim</Link>
         <Link href="/portfolio" className="hover:underline">Portfolio</Link>
       </nav>
     </header>
