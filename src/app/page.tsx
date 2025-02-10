e03f0919584c02e0f3e79d4ef2a955ceb0dd39e5
joshuajonghankim <joshuajonghankim@gmail.com>
2025-02-10 18:54:24 -0500

    update photo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,9 +13,9 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src="/next.svg"
+          src="/face.jpg"
           alt="Next.js logo"
-          width={180}
+          width={300}
           height={38}
           priority
         />
