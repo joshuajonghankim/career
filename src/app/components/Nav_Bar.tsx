4d89339ecfc744f900ef98168e56af37cde98f11
joshuajonghankim <joshuajonghankim@gmail.com>
2025-03-07 19:56:48 -0500

    update page, nav

--- src/app/components/Nav_Bar.tsx
+++ src/app/components/Nav_Bar.tsx
@@ -6,15 +6,16 @@ export default function Nav_Bar() {
       <nav className="flex gap-8 text-sm font-semibold">
         <Link href="/" className="hover:underline">Joshua Jonghan Kim</Link>
         <Link href="/portfolio" className="hover:underline">Portfolio</Link>
+        {/* print button */}
+        <button
+          onClick={() => window.print()}
+          className="hover:underline"
+        >
+          Print
+        </button>
       </nav>
 
-      {/* print button */}
-      <button
-        onClick={() => window.print()}
-        className="absolute top-4 right-4 border border-slate-950 bg-white text-black px-4 py-2 rounded-lg hover:bg-gray-200"
-      >
-        Print
-      </button>
+
     </header>
   );
 }
